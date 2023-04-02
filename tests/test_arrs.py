77419fe708782149698b9399c3e2cb76166252cd
Alexandp L <alexandp83@mail.ru>
2023-04-02 22:00:46 +0300

    Покрыл 100%

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -7,7 +7,7 @@ class TestArrs(unittest.TestCase):
     def test_get(self):
         with self.assertRaises(IndexError):
             self.assertEqual(arrs.get([1, 2, 3], 1, "test"), 2)
-            self.assertEqual(arrs.get([], 0, "test"), "test")
+            self.assertEqual(arrs.get([], -1, "test"), "test")
             self.assertEqual(arrs.get([1, 2, 3], -1, "test"), "test")
 
     def test_slice(self):
@@ -16,7 +16,3 @@ class TestArrs(unittest.TestCase):
         self.assertEqual(arrs.my_slice([], 1), [])
         self.assertEqual(arrs.my_slice([1, 2, 3], -1), [3])
         self.assertEqual(arrs.my_slice([1, 2, 3], -4), [1, 2, 3])
-
-
-if __name__ == '__main__':
-    unittest.main()
